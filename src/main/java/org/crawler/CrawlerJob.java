45546047e53154110d8563aa8b064c16edec8d6e
vitalii.levash <mr.roooot@gmail.com>
2016-10-18 22:21:32 +0300

    Initial commit

---
+++ src/main/java/org/crawler/CrawlerJob.java
@@ -0,0 +1,49 @@
+package org.crawler;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.concurrent.BrokenBarrierException;
+
+/**
+ * @author vitalii.levash
+ */
+public class CrawlerJob implements Runnable{
+     private final String url;
+     private PageCrawler pageCrawler;
+
+    public CrawlerJob(String url, PageCrawler pageCrawler) {
+        this.url = url;
+        this.pageCrawler = pageCrawler;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Document doc = Jsoup.connect(url).get();
+            Elements links =doc.getElementsByTag("a");
+
+            links.parallelStream()
+                    .forEach(link-> {
+                        pageCrawler.addUrl(link.attr("href"));
+                        System.out.println("-"+link.attr("href"));
+                    });
+             if (pageCrawler.getBarrier().getNumberWaiting()==1){
+                 pageCrawler.getBarrier().await();
+             }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
---
+++ src/main/java/org/crawler/PageCrawler.java
@@ -0,0 +1,70 @@
+package org.crawler;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.*;
+
+/**
+ * @author vitalii.levash
+ */
+public class PageCrawler {
+
+    private BlockingQueue<String> link = new LinkedBlockingQueue<>();
+    private CyclicBarrier barrier = new CyclicBarrier(2);
+    private Set<String> result = new HashSet<>();
+
+    private ExecutorService executorService;
+    private final String url;
+
+    public PageCrawler(String url) {
+        this.url = url;
+    }
+
+    public void addUrl(String url){
+        link.add(url);
+    }
+
+    public CyclicBarrier getBarrier() {
+        return barrier;
+    }
+
+    public void setBarrier(CyclicBarrier barrier) {
+        this.barrier = barrier;
+    }
+
+    public void start() throws InterruptedException {
+        String next = null;
+        executorService = Executors.newCachedThreadPool();
+        while(!link.isEmpty()){
+            next = link.take();
+
+            if (next==null){
+                System.out.println("Queue is empty");
+            }
+
+            if (!checkUrl(next)) continue;
+
+            result.add(next);
+            System.out.println(next);
+
+            CrawlerJob job = new CrawlerJob(next,this);
+            executorService.submit(job);
+        }
+    }
+
+    private boolean checkUrl(String nextUrl) {
+
+        if(result.contains(nextUrl)) { return false; }
+        if(nextUrl.startsWith("javascript:"))  { return false; }
+        if(nextUrl.startsWith("#"))            { return false; }
+        if(nextUrl.endsWith(".swf"))           { return false; }
+        if(nextUrl.endsWith(".pdf"))           { return false; }
+        if(nextUrl.endsWith(".png"))           { return false; }
+        if(nextUrl.endsWith(".gif"))           { return false; }
+        if(nextUrl.endsWith(".jpg"))           { return false; }
+        if(nextUrl.endsWith(".jpeg"))          { return false; }
+
+        return true;
+    }
+
+}
